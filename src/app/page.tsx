b43974862f7067ee87d9d67ec780201ff4b98627
brian-ambani <brainambani1@gmail.com>
2023-12-30 01:55:35 +0300

    updated the the features secction title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,6 +102,18 @@ export default function Home() {
       </div>
 
       {/* Feature section */}
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-800 sm:text-5xl">
+              Start Chatting in minutes
+            </h2>
+            <p className="mt-4 text-large text-gray-600">
+              Chatting to your PDF files has been easier than with Scriptify.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
