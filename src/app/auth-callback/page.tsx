db27ad0e68550154afbf98e937c915abe5ab3192
brian-ambani <brainambani1@gmail.com>
2023-12-31 03:33:02 +0300

    updated the authcall back

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,10 +1,21 @@
 import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
 
-const Page = () => {
+const Page = async () => {
   const router = useRouter();
 
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
+
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }: { success: boolean }) => {
+      if (success) {
+        //user is synced to db
+        // Redirect to origin or home page
+        router.push(origin ? `/${origin}` : "/dashboard");
+      }
+    },
+  });
 };
 
 export default Page;
