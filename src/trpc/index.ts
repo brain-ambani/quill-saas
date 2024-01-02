0b83db9a2188b8f914706790a44b98bb24936a98
brian-ambani <brainambani1@gmail.com>
2024-01-02 12:46:02 +0300

    updated the index file

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,18 +1,33 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = await getUser();
 
-    if (!user?.id || !user?.email) {
+    if (!user?.id || !user?.email)
       throw new TRPCError({ code: "UNAUTHORIZED" });
 
-      // Check if user is in the database
-      return { success: true };
+    // Check if user is in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+    if (!dbUser) {
+      // Create user in database
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
     }
+
+    return { success: true };
   }),
 });
 
